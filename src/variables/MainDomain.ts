87edad209fa4f26ba7b5411cafd2dcf0c0248100
ibrahimsaleh8 <ebrihm576@gmail.com>
2025-09-19 19:58:12 +0300

    add new main domain after deployment

--- src/variables/MainDomain.ts
+++ src/variables/MainDomain.ts
@@ -1,1 +1,2 @@
-export const MainDomain = "http://localhost:3000";
+// export const MainDomain = "http://localhost:3000";
+export const MainDomain = "https://university-management-system-eta.vercel.app";
